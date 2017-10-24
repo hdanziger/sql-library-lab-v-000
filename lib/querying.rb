331b30bd3652c6534f5ea3c3c1dcfa3b9dc360e7
Hannah Danziger <hdanziger8@gmail.com>
2017-10-24 19:23:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name JOIN character_books ON character_books.book_id = books.id;"
 end
